21f86901c8097996ada2cea9bbbfed807cfa042a
luisdt <luis.ernesto951008@gmail.com>
2020-05-18 16:03:47 -0300

    Remove input receiver

--- run_squad.py
+++ run_squad.py
@@ -1275,26 +1275,26 @@ def main(_):
                           FLAGS.do_lower_case, output_prediction_file,
                           output_nbest_file, output_null_log_odds_file)
 
-        estimator._export_to_tpu = False  ## !!important to add this
-        estimator.export_saved_model(
-            export_dir_base='saved_model',
-            serving_input_receiver_fn=serving_input_receiver_fn)
+    estimator._export_to_tpu = False  ## !!important to add this
+    estimator.export_saved_model(
+        export_dir_base='saved_model',
+    )
 
 
-def serving_input_receiver_fn():
-    feature_spec = {
-        "unique_ids": tf.FixedLenFeature([], tf.int64),
-        "input_ids": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
-        "input_mask": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
-        "segment_ids": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
-    }
-
-    serialized_tf_example = tf.placeholder(dtype=tf.string,
-                                           shape=[FLAGS.train_batch_size],
-                                           name='input_example_tensor')
-    receiver_tensors = {'examples': serialized_tf_example}
-    features = tf.parse_example(serialized_tf_example, feature_spec)
-    return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
+# def serving_input_receiver_fn():
+#     feature_spec = {
+#         "unique_ids": tf.FixedLenFeature([], tf.int64),
+#         "input_ids": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
+#         "input_mask": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
+#         "segment_ids": tf.FixedLenFeature([FLAGS.max_seq_length], tf.int64),
+#     }
+#
+#     serialized_tf_example = tf.placeholder(dtype=tf.string,
+#                                            shape=[FLAGS.train_batch_size],
+#                                            name='input_example_tensor')
+#     receiver_tensors = {'examples': serialized_tf_example}
+#     features = tf.parse_example(serialized_tf_example, feature_spec)
+#     return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
 
 
 if __name__ == "__main__":
